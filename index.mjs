1f395bd1592c64f552b5bcbb14e245c28c5559e7
zayne <zayne@toeverykid.org>
2021-10-22 19:51:53 -0700

    dotenv update

--- index.mjs
+++ index.mjs
@@ -1,24 +1,28 @@
 import express from 'express'
-import { Sequelize } from 'sequelize'
-import usercontroller from './controllers/usercontroller.mjs';
+import dotenv from 'dotenv'
+dotenv.config()
+// import { Sequelize } from 'sequelize'
+import usercontroller from './controllers/usercontroller.mjs'
 
+// Creating Express App
 const app = express()
 const port = 3000
 app.use(express.json())
 
-const sequelize = new Sequelize('project_calculator', 'root', '', {
+// Making Database Connection (Sequelize CLI)
+// When installing use node_modules/.bin/sequelize init
+/*const sequelize = new Sequelize('project_calculator', 'root', '', {
   host: 'localhost',
   dialect: 'mariadb'
 })
 
-
 try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully.');
+  await sequelize.authenticate()
+  console.log('Connection has been established successfully.')
 } catch (error) {
-  console.error('Unable to connect to the database:', error);
+  console.error('Unable to connect to the database:', error)
 }
-
+*/
 app.get('/', (req, res) => {
   res.send({ greeting: 'Hello World!' })
 })
