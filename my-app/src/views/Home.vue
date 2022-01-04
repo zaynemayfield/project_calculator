05e720a08bc87ec73e4f3fe390d9a5488615cd9b
whitegumby <zaynemayfield@gmail.com>
2022-01-04 16:36:32 -0600

    Add link to new project and title if projects

--- my-app/src/views/Home.vue
+++ my-app/src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
 <div>
-<h4 v-if="!projects.length">No projects yet</h4>
+
+    <router-link :to="{ name: 'New Project' }">New Project</router-link>
+
+<h4 v-if="!projects.length">No Projects Yet</h4>
+<h4 v-if="projects.length">Your Projects</h4>
 <ul v-if="projects.length">
+  <!-- make the link clickable -->
   <li :key="index" v-for="(project, index) in projects">{{ project.name }}</li>
 </ul>
 </div>
