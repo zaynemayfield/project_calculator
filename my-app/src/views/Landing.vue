deea1127e5cb6be6e9fad566e457ae4cdf643660
whitegumby <zaynemayfield@gmail.com>
2022-01-05 15:35:09 -0600

    attempt at making link to go to view Project

--- my-app/src/views/Landing.vue
+++ my-app/src/views/Landing.vue
@@ -2,14 +2,15 @@
 <div>
 <h4>Welcome to Projects!</h4>
 <p>This web app gives you the power to create projects and quote them completely out! Share them with friends and update the prices and materials</p>
-<h5>Latest Public Projects</h5>
+<h3>Latest Public Projects</h3>
 <div>
 <h4 v-if="!projects.length">No projects yet</h4>
 <ul v-if="projects.length">
   <li :key="index" v-for="(project, index) in projects">
       <!-- SHow avatar and make it a link to a page that a non-auth user can view -->
+       <a href="" @click="goToPublicProject(project.id)"  ><h5>{{ project.name }} <br>
+       {{ project.summary }}</h5></a>
        
-       {{ project.name }}
        
        </li>
 </ul>
@@ -25,10 +26,16 @@ export default {
     const projects = ref([])
     const getPublicProjects = async () => {
       projects.value = await apiClient.getPublicProjects()
+    
+
     }
+
     onMounted(getPublicProjects)
     return {
-      projects
+      projects,
+      goToPublicProject(id) {
+        this.router.push({ name: 'View Project', Params: {projectId: id}})
+      }
     }
   }
 }
