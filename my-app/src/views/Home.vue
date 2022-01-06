13b17dc96ef1d02a23e3049f61bce0992e443110
whitegumby <zaynemayfield@gmail.com>
2022-01-05 20:41:28 -0600

    change the Create New Project link and placement

--- my-app/src/views/Home.vue
+++ my-app/src/views/Home.vue
@@ -1,10 +1,20 @@
 <template>
 <div>
 
-    <router-link :to="{ name: 'New Project' }">New Project</router-link>
 
-<h4 v-if="!projects.length">No Projects Yet</h4>
-<h4 v-if="projects.length">Your Projects</h4>
+<div class="row">
+
+  <div class="col">
+    <h4 v-if="!projects.length">No Projects Yet</h4>
+    <h4 v-if="projects.length">Your Projects</h4>
+  </div>
+
+  <div class="col">
+    <button type="button" class="btn btn-primary" @click="redirect('New Project','')">Create New Project</button>
+  </div>
+  
+</div>
+
 <ul v-if="projects.length">
   <!-- make the link clickable -->
   <li :key="index" v-for="(project, index) in projects"> <a href="" @click.prevent="redirect('Design Project', project.id)"> <strong>{{ project.name }}</strong> - {{ project.summary }}</a></li>
