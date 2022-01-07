cc65caeea22a718360e11c52649fe9783d1653b1
whitegumby <zaynemayfield@gmail.com>
2022-01-07 16:19:04 -0600

    added showing project info on project design page

--- my-app/src/views/ProjectDesign.vue
+++ my-app/src/views/ProjectDesign.vue
@@ -1,3 +1,30 @@
 <template>
-    <div></div>
+    <div>
+        <div class="row">
+            <div class="col">{{ project.name }}</div>
+            <div class="col">{{ project.summary }}</div>
+        </div>
+
+    </div>
 </template>
+
+<script>
+import { inject, onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router';
+export default {
+    setup() {
+        const route = useRoute();
+        const projectId = route.params.id
+        console.log(projectId)
+        const apiClient = inject('$api', {})
+        const project = ref([])
+        const getProject = async () => {
+        project.value = await apiClient.getProject(projectId)
+        }
+        onMounted(getProject)
+        return {
+            project,
+        }
+}
+}
+</script>
