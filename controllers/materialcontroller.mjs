ed1482718cda2f5f64038a10334d3e13add9cfcd
whitegumby <zaynemayfield@gmail.com>
2022-01-12 00:26:46 -0600

    Added ReadAll method

--- controllers/materialcontroller.mjs
+++ controllers/materialcontroller.mjs
@@ -2,6 +2,7 @@ import pkg from '@prisma/client'
 const { PrismaClient } = pkg
 const prisma = new PrismaClient()
 import Helper from '../utilities/helper.mjs'
+import lineitemcontroller from './lineitemcontroller.mjs'
 
 class MaterialController {
   
@@ -11,14 +12,16 @@ async create (req, res) {
     if (name.length < 0){
       return new Helper(res).sendError("No Name Entered", 'name')
     }
-    const user_id = req.body.user_id
+    const user_id = req.user._id
     const url = req.body.url
     const price = req.body.price
     const description = req.body.description
-      const material = await prisma.material.create({ data: { name: name, user_id: user_id, url: url, price: price, description: description } })
-      delete material.dataValues.user_id
+    const project_id = parseInt(req.body.project_id)
+      const material = await prisma.material.create({ data: { name: name, user_id: user_id, url: url, price: price, description: description, project_id: project_id } })
+      const blankLineItem = await lineitemcontroller.createBlank(material.user_id, material.project_id, material.id)
       return res.send({material: material})
   } catch (error) {
+    console.log(error)
     return res.status(500).send({ errors: error.errors.map(error => { return { message: error.message, field: error.path } }) })
   }
 }
@@ -31,6 +34,19 @@ async read (req, res) {
   }
   delete material.dataValues.user_id
   return res.send({material: material})
+}
+
+async readAll (req, res) {
+  const project_id = parseInt(req.params.id)
+  const materials = await prisma.material.findMany({ where: {project_id: project_id}})
+  if (!materials) {
+    return new Helper(res).sendError('No material with that ID Exists', 'id')
+  }
+  return res.send({material: materials})
+}
+
+async update (req, res) {
+
 }
 
 async delete (req, res) {
