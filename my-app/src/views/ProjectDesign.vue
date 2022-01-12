3e82405ad9a5feafa7a112e1e72191b31d7d4ffd
whitegumby <zaynemayfield@gmail.com>
2022-01-12 00:29:02 -0600

    working w/ database, got data, but no showing it

--- my-app/src/views/ProjectDesign.vue
+++ my-app/src/views/ProjectDesign.vue
@@ -9,9 +9,9 @@
 
 <!-- Create New Material is here ONLY SHOW WHEN NOT UPDATING MATERIAL -->
 <!-- Create Line item when material is created -->
- <div class="card">
+ <div class="card" v-if="!showEditMaterialForm">
     <div class="card-body">
-      <form enctype="application/x-www-form-urlencoded" @submit.prevent="createMaterial">
+      <form enctype="multipart/form-data" @submit.prevent="createMaterial">
       <div class="row  align-items-center">
           <div class="col">
             <!-- NAME FIELD -->
@@ -30,6 +30,7 @@
           <input type="number" class="form-control" min="0.00" step="0.01" id="price" name="price" placeholder="0.00" />
         </div>
         <div class="col">
+            <input type="hidden" name="project_id" :value="project.id">
             <!-- SUBMIT -->
             <button type="submit" class="btn btn-success">Add Material</button>
         </div>
@@ -41,28 +42,29 @@
 
 <!-- Update New Material is here ONLY SHOW WHEN CLICK EDIT MATERIAL -->
 <!-- Update Material only -->
- <div class="card">
+ <div class="card" v-if="showEditMaterialForm">
     <div class="card-body">
       
       <form enctype="application/x-www-form-urlencoded" @submit.prevent="updateMaterial">
       <div class="row align-items-center">
           <div class="col">
             <!-- NAME FIELD -->
-          <input type="text" class="form-control" id="name" name="name" placeholder="Name" required />
+          <input type="text" class="form-control" id="name" name="name" placeholder="Name" value="" required />
         </div>
         <div class="col">
             <!-- URL FIELD -->
-          <input type="url" class="form-control" id="url" name="url" placeholder="URL" />
+          <input type="url" class="form-control" id="url" name="url" placeholder="URL"  value="" />
         </div>
         <div class="col">
             <!-- DESCRIPTION FIELD -->
-          <input type="text" class="form-control" id="description" name="description" placeholder="Description" />
+          <input type="text" class="form-control" id="description" name="description" placeholder="Description"  value="" />
         </div>
         <div class="col">
             <!-- PRICE FIELD -->
-          <input type="number" class="form-control" min="0.00" step="0.01" id="price" name="price" placeholder="0.00" />
+          <input type="number" class="form-control" min="0.00" step="0.01" id="price" name="price" placeholder="0.00"  value="" />
         </div>
         <div class="col">
+            <input type="hidden" name="id" value="">
             <!-- SUBMIT -->
             <button type="submit" class="btn btn-primary">Update Material</button>
         </div>
@@ -80,10 +82,10 @@
             <h5>Project Info:</h5>
         </div>
         <div class="col">
-            Total Project Cost:
+            Total Project Cost: {{ totalProjectCost() }}
         </div>
         <div class="col">
-            Number of Items:
+            Number of Items: {{ lineItems.length }}
         </div>
     </div>
 </div>
@@ -91,39 +93,39 @@
 <!-- End Project Information -->
 
 <!-- If no Materials -->
-<div class="border rounded p-3">
+<div class="border rounded p-3" v-if="!materials">
     <h4>Create a Material to get started!</h4>
 </div>
 <!-- END no Materials -->
 
 <!-- Line Item -->
 <!-- Update line Item without clicking submit, just have it saved automatically -->
-<div class="border rounded p-3">
+<div class="border rounded p-3" :key="index" v-for="(lineItem, index) in lineItems">
     <!-- TOP ROW -->
         <div class="row  align-items-center mb-2">
             <!-- NAME -->
             <div class="col">
-                material.name
+                {{ getMaterial(lineItem.material).name }}
             </div>
 
             <!-- URL -->
             <div class="col">
-                material.url
+                <a :href="getMaterial(lineItem.material).url" target="_blank"> {{  getMaterial(lineItem.material).url }} </a>
             </div>
 
             <!-- DESCRIPTION -->
             <div class="col">
-                material.description
+                {{  getMaterial(lineItem.material).description }}
             </div>
 
             <!-- EDIT MATERIAL -->
             <div class="col text-end">
-                <button type="submit" class="btn btn-primary">Edit Material</button>
+                <button @click="beginMaterialEdit" type="submit" class="btn btn-primary">Edit Material</button>
             </div>
 
             <!-- DELETE MATERIAL -->
             <div class="col">
-                <button type="submit" class="btn btn-danger">Delete Material</button>
+                <button type="submit" class="btn btn-danger" @click="deleteMaterial(lineItem.material)">Delete Material</button>
             </div>
         </div>
         <!-- BOTTOM ROW -->
@@ -131,27 +133,27 @@
 
             <!-- NOTES -->
             <div class="col">
-                <input type="text" class="form-control" id="notes" name="notes" placeholder="Notes" value="lineItem.notes" />
+                <input type="text" class="form-control" id="notes" name="notes" placeholder="Notes" v-model="lineItem.notes" />
             </div>
             
             <!-- PRICE -->
             <div class="col">
-                material.price
+                {{  financialFormat(getMaterial(lineItem.material).price) }}
             </div>
 
             <!-- QUANTITY -->
             <div class="col">
-                <input type="number" class="form-control" min=".01" step="0.01" id="quantity" name="quantity" placeholder="1.00" value="lineItem.quantity" />
+                <input type="number" class="form-control" min="0" step="1" id="quantity" name="quantity" placeholder="Quantity" v-model="lineItem.quantity" />
             </div>
 
             <!-- TOTAL -->
             <div class="col">
-                $Total
+                {{financialFormat(lineItem.quantity * getMaterial(lineItem.material).price) }}
             </div>
 
             <!-- DUPLICATE LINE ITEM -->
             <div class="col">
-                <button type="submit" class="btn btn-success">Duplicate Item</button>
+                <button @click="duplicateLineItem(index)" type="submit" class="btn btn-success">Duplicate Item</button>
             </div>
         </div>
         </div>
@@ -169,16 +171,83 @@ export default {
     setup() {
         const route = useRoute();
         const projectId = route.params.id
-        console.log(projectId)
         const apiClient = inject('$api', {})
-        const project = ref([])
-        const getProject = async () => {
-        project.value = await apiClient.getProject(projectId)
+
+        const project = ref({})
+        const materials = ref([])
+        const showEditMaterialForm = ref(false)
+        const lineItems = ref([])
+
+        const beginMaterialEdit = () => {
+            showEditMaterialForm.value = true
         }
+
+        const getProject = async () => {
+            project.value = await apiClient.getProject(projectId)
+        }
+
+        const getMaterials = async () => {
+            materials.value = await apiClient.getMaterials(projectId)
+        }
+
+        const getLineItems = async () => {
+            lineItems.value = await apiClient.getLineItems(projectId)
+            console.log(lineItems)
+        }
+
+        const getMaterial = (index) => {
+            return materials.value[index]
+        }
+
+        const updateMaterial = () => {
+        // API Call here to update material
+        //if call successful, then:
+        showEditMaterialForm.value = false
+        }
+
+        const deleteMaterial = (materialIndex) => {
+            lineItems.value.splice(materialIndex, 1)
+        } 
+
+        const duplicateLineItem = (index) => {
+            const itemToDuplicate = { ...lineItems.value[index] }
+            lineItems.value.push(itemToDuplicate)
+        }
+
+
+        const financialFormat = (number) => {
+            return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }). format(number)
+        }
+
+        const totalProjectCost = () => {
+            let totalCost = 0
+            for (const lineItem of lineItems.value) {
+                totalCost += (lineItem.quantity * getMaterial(lineItem.material).price)
+            }
+            return financialFormat(totalCost)
+        }
+
+
         onMounted(getProject)
+        onMounted(getMaterials)
+        onMounted(getLineItems)
         return {
             project,
+            showEditMaterialForm,
+            updateMaterial,
+            beginMaterialEdit,
+            materials,
+            deleteMaterial,
+            lineItems,
+            getMaterial,
+            duplicateLineItem,
+            financialFormat,
+            totalProjectCost,
+            createMaterial: (e) => {
+                const data = new FormData(e.target)
+                apiClient.createMaterial(data)
+            }
         }
-}
+    }
 }
 </script>
