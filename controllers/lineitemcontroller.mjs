fc3301de0220786f38c9d57d8e2044a03178f8e7
whitegumby <zaynemayfield@gmail.com>
2022-01-14 15:50:32 -0600

    Add duplicate, update, delete

--- controllers/lineitemcontroller.mjs
+++ controllers/lineitemcontroller.mjs
@@ -8,7 +8,6 @@ class LineItemController {
 async createBlank (user, project, material) {
   try {
     const blankLineItem = await prisma.line_item.create({ data: { user_id: user, project_id: project, material_id: material} })
-    console.log('Created blank line item')
     return blankLineItem
   } catch (error) {
     console.log(error)
@@ -33,6 +32,23 @@ async create (req, res) {
   }
 }
 
+async duplicate (req, res) {
+  try {
+    const id = parseInt(req.params.id)
+    const lineItem = await prisma.line_item.findUnique({ where: {id: id}})
+    if (!lineItem) {
+      return
+    }
+    const duplicateLineItem = await prisma.line_item.create({ data: { quantity: lineItem.quantity, user_id: lineItem.user_id, notes: lineItem.notes, project_id: lineItem.project_id, material_id: lineItem.material_id } })
+    if (!duplicateLineItem) {
+      return
+    }
+    return res.send({duplicateLineItem: duplicateLineItem})
+  } catch (error) {
+    console.log(error)
+  }
+}
+
 async read (req, res) {
   const id = parseInt(req.params.id)
   const material = await prisma.material.findUnique({ where: {id: id}})
@@ -43,37 +59,55 @@ async read (req, res) {
   return res.send({material: material})
 }
 
+async both (req, res) {
+  const project_id = parseInt(req.params.id)
+  const materialLine = await prisma.line_item.findMany({ where: { project_id: project_id}, include: { material: { select: { name: true, url: true, price: true, description: true }}}, orderBy: { updatedAt: 'desc' }})
+  if (!materialLine) {
+    return new Helper(res).sendError('No material and Line Item with that ID Exists', 'id')
+  }
+  return res.send({materialLine: materialLine})
+}
+
 async readAll (req, res) {
   const project_id = parseInt(req.params.id)
   const lineItems = await prisma.line_item.findMany({ where: {project_id: project_id}})
   if (!lineItems) {
     return new Helper(res).sendError('No line items exist on project', 'id')
   }
-  console.log(lineItems)
+  //console.log(lineItems)
   return res.send({lineItem: lineItems})
 }
 
 async update (req, res) {
-
+  const id = parseInt(req.body.id)
+  const quantity = parseInt(req.body.quantity)
+  const updateLineItem = await prisma.line_item.update({
+    where: {
+      id: id,
+    },
+    data: {
+      quantity: quantity,
+      notes: req.body.notes
+    },
+  })
+  if (!updateLineItem) {
+    return new Helper(res).sendError('Line Item failed to update', 'id')
+  }
+  return res.send({updateLineItem: updateLineItem})
 }
 
 async delete (req, res) {
   const id = parseInt(req.params.id)
-  const materialid = await prisma.material.findUnique({ where: {id: id}})
-  if (!materialid) {
-    return new Helper(res).sendError('No material with that ID Exists', 'id')
-  }
   try {
-    await prisma.material.delete({
-      where: { id: materialid.id },
-      data: { delete: 'Y'}
-    })
+  const lineItem = await prisma.line_item.delete({ where: {id: id}})
+  const deleteLineItem = id
+  return res.send({deleteLineItem: deleteLineItem})
   } catch (error) {
-    return res.status(500).send({ errors: error.errors.map(error => { return { message: error.message, field: error.path } }) })
+    console.log(error)
   }
   
   
-  return res.send({materialid: materialid})
+  
 }
 
 }
